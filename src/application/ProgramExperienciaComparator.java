114d66478dfcc5afc3ffa2dbb95aa266647af786
aschaves1976 <roots17@gmail.com>
2018-10-11 10:20:45 -0300

    Experiência com Comparator - Quarta forma (Expressões Lambda - Melhorando a sintaxe)

--- src/application/ProgramExperienciaComparator.java
+++ src/application/ProgramExperienciaComparator.java
@@ -15,8 +15,8 @@ public class ProgramExperienciaComparator {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		/**
-		 * 1 - Vamos ignorar a classe MyComparator(); 2 - Vamos declarar uma variável do
-		 * tipo Comparator, utilizando uma sintaxe de classe anônima;
+		 * - 01 1 - Vamos ignorar a classe MyComparator(); 2 - Vamos declarar uma
+		 * variável do tipo Comparator, utilizando uma sintaxe de classe anônima;
 		 * 
 		 * 
 		 * /* Comparator<Product> comp = new Comparator<Product>() {
@@ -27,14 +27,20 @@ public class ProgramExperienciaComparator {
 		 * 
 		 *           };
 		 */
-		/*
-		 * A utilização da classe anônima, da forma como foi implementada, ficou muito
-		 * grande, muito verbosa. Vamos utilizar Expressões Lambda - que é uma função
-		 * anônima -, para deixar a declaração da classe mais enxuta.
+
+		/**
+		 * - 02 A utilização da classe anônima, da forma como foi implementada, ficou
+		 * muito grande, muito verbosa. Vamos utilizar Expressões Lambda - que é uma
+		 * função anônima -, para deixar a declaração da classe mais enxuta.
+		 *
+		 * Comparator<Product> comp = (p1, p2) -> { return
+		 * p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()); };
 		 */
-		Comparator<Product> comp = (p1, p2) -> {
-			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-		};
+
+		/**
+		 * - 03 Melhorando a sintaxe.
+		 */
+		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 
 		// list.sort(new MyComparator());
 		list.sort(comp);
