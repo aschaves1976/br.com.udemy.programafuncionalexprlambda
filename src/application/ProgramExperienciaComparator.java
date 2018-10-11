0984f06fcd8943bd6e7d281f48d51d2de69b4958
aschaves1976 <roots17@gmail.com>
2018-10-11 10:14:09 -0300

    Experiência com Comparator - Terceira forma (Utilizando Expressões Lambda)

--- src/application/ProgramExperienciaComparator.java
+++ src/application/ProgramExperienciaComparator.java
@@ -17,16 +17,25 @@ public class ProgramExperienciaComparator {
 		/**
 		 * 1 - Vamos ignorar a classe MyComparator(); 2 - Vamos declarar uma variável do
 		 * tipo Comparator, utilizando uma sintaxe de classe anônima;
+		 * 
+		 * 
+		 * /* Comparator<Product> comp = new Comparator<Product>() {
+		 * 
+		 * @Override public int compare(Product p1, Product p2) { // TODO Auto-generated
+		 *           method stub return
+		 *           p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()); }
+		 * 
+		 *           };
 		 */
-		Comparator<Product> comp = new Comparator<Product>() {
-
-			@Override
-			public int compare(Product p1, Product p2) {
-				// TODO Auto-generated method stub
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			}
-
+		/*
+		 * A utilização da classe anônima, da forma como foi implementada, ficou muito
+		 * grande, muito verbosa. Vamos utilizar Expressões Lambda - que é uma função
+		 * anônima -, para deixar a declaração da classe mais enxuta.
+		 */
+		Comparator<Product> comp = (p1, p2) -> {
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		};
+
 		// list.sort(new MyComparator());
 		list.sort(comp);
 
