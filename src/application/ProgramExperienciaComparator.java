99ca8e9947aaa8a1a6b17af6f6b08cd3602b6a1d
aschaves1976 <roots17@gmail.com>
2018-10-11 10:02:50 -0300

    Experiência com Comparator - Segunda forma de utilização (Utilizando classe anônima

--- src/application/ProgramExperienciaComparator.java
+++ src/application/ProgramExperienciaComparator.java
@@ -1,10 +1,10 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
-import service.MyComparator;
 
 public class ProgramExperienciaComparator {
 
@@ -14,8 +14,21 @@ public class ProgramExperienciaComparator {
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
+		/**
+		 * 1 - Vamos ignorar a classe MyComparator(); 2 - Vamos declarar uma variável do
+		 * tipo Comparator, utilizando uma sintaxe de classe anônima;
+		 */
+		Comparator<Product> comp = new Comparator<Product>() {
 
-		list.sort(new MyComparator());
+			@Override
+			public int compare(Product p1, Product p2) {
+				// TODO Auto-generated method stub
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+
+		};
+		// list.sort(new MyComparator());
+		list.sort(comp);
 
 		for (Product p : list) {
 			System.out.println(p);
