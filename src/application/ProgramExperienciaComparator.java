89071c09f05b93499df9ac8abc7b182dc8e7ee3d
aschaves1976 <roots17@gmail.com>
2018-10-11 09:51:01 -0300

    Experiência com Comparator - Primeira forma de utilização

--- src/application/ProgramExperienciaComparator.java
+++ src/application/ProgramExperienciaComparator.java
@@ -1,9 +1,25 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+import service.MyComparator;
+
 public class ProgramExperienciaComparator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		List<Product> list = new ArrayList<>();
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+
+		list.sort(new MyComparator());
+
+		for (Product p : list) {
+			System.out.println(p);
+		}
 
 	}
 
